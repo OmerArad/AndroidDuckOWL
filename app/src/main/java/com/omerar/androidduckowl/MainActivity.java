b3d48c2077f8f8d4125dcf6b16f2f6c0d07481e8
Omer Arad <OMERAR@il.ibm.com>
2019-03-01 09:56:16 +0200

    Now the MainActivity screen will update automatically if connected to the Wifi

--- app/src/main/java/com/omerar/androidduckowl/MainActivity.java
+++ app/src/main/java/com/omerar/androidduckowl/MainActivity.java
@@ -313,27 +313,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent)
         {
-            try
-            {
-                if (isOnline(context)) {
-                    checkConnectionStatus();
-                } else {
-                    checkConnectionStatus();
-                }
-            } catch (NullPointerException e) {
-                e.printStackTrace();
-            }
-        }
-
-        private boolean isOnline(Context context) {
             try {
-                ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-                NetworkInfo netInfo = cm.getActiveNetworkInfo();
-                //should check null because in airplane mode it will be null
-                return (netInfo != null && netInfo.isConnected());
+                checkConnectionStatus();
             } catch (NullPointerException e) {
                 e.printStackTrace();
-                return false;
             }
         }
     }
